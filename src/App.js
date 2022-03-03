e81dd49ed85319dfac5f9c3613ce9789cddc4318
ajay9702@gmail.com <ajay9702@gmail.com>
2022-03-03 13:43:39 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
+import TodoItem from './components/TodoItem';
+import { useSelector } from 'react-redux'
+import { selectTodoList } from './features/todoSlice'
+
 
 function App() {
+  const todoList = useSelector(selectTodoList)
   return (
-    <div className="App">
-      <h1>hello world
-        
-      </h1>
+    <div className="app">
+      
+      <div className='app__container'>
+      <Input />
+      <div className='app__todoContainer'>
+        {
+          todoList.map(item =>
+           <TodoItem
+           name={item.item}
+           done={item.done}
+           id={item.id}
+           /> 
+            )
+        }
+      </div>
+      
+      </div>
     </div>
   );
 }
--- src/components/input.js
+++ src/components/input.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react'
 import './Input.css'
 
+import  { useDispatch } from 'react-redux'
+import { saveTodo } from '../features/todoSlice'
+
 const Input = () => {
     const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
     const addTodo =() => {
+        console.log(`adding ${input}`)
+
+        dispatch(saveTodo({
+            item: input,
+            done: false,
+            id: Date.now()
+        }))
 
     }
   return (
     <div className='input'>
-        <input type="text"/>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
         <button onClick={addTodo}>ADD</button>
     </div>
   )
